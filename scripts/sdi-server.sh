ed7574ab876a6fd96bc11e3dc73c90334f39f5e4
Antonio Negro <anegro.floss@gmail.com>
2016-01-26 10:38:08 +0100

    Arreglo IPv4/IPv6

--- scripts/sdi-server.sh
+++ scripts/sdi-server.sh
@@ -15,9 +15,12 @@ source /home/scripts/sdi.conf
 #
 ########
 
+# Averiguar IPv4
+IP=`ip -4 a | grep 192 | awk '{print $2}' | cut -d "/" -f 1`
+
 # Publicar servicio con Zeroconf
 HOST=`hostname`
-avahi-publish -s $DEVICE _vnc._tcp $PORT "Solución Digital Integrada ($HOST)" &
+avahi-publish -s $DEVICE _vnc._tcp $PORT "Solución Digital Integrada ($IP)" &
 
 # Lanzar VNC
 x11vnc -passwd "$AUTH" -rfbport $PORT -forever &
