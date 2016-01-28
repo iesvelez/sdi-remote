521b8d95a867cf645991e006438dcfdcf303dbfa
Antonio Negro <anegro.floss@gmail.com>
2016-01-28 09:04:36 +0100

    Ajustes IPv4/IPv6 y asistente config

--- scripts/sdi-client.sh
+++ scripts/sdi-client.sh
@@ -18,8 +18,8 @@ source /home/scripts/sdi.conf
 #SERVER=`avahi-browse -trpk _vnc._tcp | grep $DEVICE | grep = | grep IPv4 | cut -d ";" -f 8`
 #PORT=`avahi-browse -trpk _vnc._tcp | grep $DEVICE | grep = | grep IPv4 | cut -d ";" -f 9`
 
-SERVER=`avahi-browse -trpk _vnc._tcp | grep $DEVICE | grep = | grep IPv6 | cut -d "(" -f 2 | cut -d ")" -f 1`
-PORT=`avahi-browse -trpk _vnc._tcp | grep $DEVICE | grep = | grep IPv6 | cut -d ";" -f 9`
+SERVER=`avahi-browse -trpk _vnc._tcp | grep $DEVICE | grep = | tail -n 1 | cut -d "(" -f 2 | cut -d ")" -f 1`
+PORT=`avahi-browse -trpk _vnc._tcp | grep $DEVICE | grep = | tail -n 1 | cut -d ";" -f 9`
 
 echo "Conectando con $SERVER:$PORT..."
 echo $AUTH | xtightvncviewer -autopass $SERVER:$PORT
